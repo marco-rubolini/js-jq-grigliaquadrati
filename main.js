c83ac8dd0e307bacf413812d0d7b3db9e977989e
Marco <rubolini.marco@gmail.com>
2020-05-07 15:46:42 +0200

    Genera e scrivi numero random

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-//Creare in HTML una griglia di 5x5 quadrati vuoti.
 // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
+
+$('.quadrato').each(function(){
+    var numeroRandom = generaNumeroRandom(0, 10);
+    console.log(numeroRandom);
+    $(this).text(numeroRandom);
+})
+
+
+// Scrivo la funzione per generare un numero random tra min e max
+function generaNumeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
